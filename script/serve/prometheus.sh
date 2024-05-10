5fccdc0816f5fa53b4dc62eac105bfe13641e396
HeegyuKim <heekue83@gmail.com>
2024-05-10 06:44:21 +0000

    시퀀스 길이 이슈

--- script/serve/prometheus.sh
+++ script/serve/prometheus.sh
@@ -1,3 +1,5 @@
 
 python -m tuna.serve.fastapi_flax \
-    --model_name prometheus-eval/prometheus-7b-v2.0
+    --model_name prometheus-eval/prometheus-7b-v2.0 \
+    --prompt_length 3072 \
+    --max_new_tokens 1024
