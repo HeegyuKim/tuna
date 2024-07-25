c8e5c5dfc5926677b3c70f51db6211557d84576a
HeegyuKim <heekue83@gmail.com>
2024-07-25 15:40:26 +0000

    gemma2 it

--- flax-script/ko-chat/gemma2.sh
+++ flax-script/ko-chat/gemma2.sh
@@ -21,18 +21,20 @@ train() {
         --max_length=2048 \
         --model_name_or_path $model \
         --total_epochs 2 \
+        --logging_steps 2048 \
         --learning_rate $lr \
         --last_learning_rate_ratio 0.3 \
         --lr_warmup_ratio 0.01 \
         --lr_scheduler cosine \
         --load_from_cache_file \
         --train_template $template \
-        --train_total_batch_size 32 \
+        --train_total_batch_size 128 \
         --train_batch_size_per_device 1 \
         --eval_batch_size_per_device 1 \
         --push_to_hub \
         --push_to_hub_id $run_name \
         --save_strategy epoch \
+        --save_epochs 2 \
         --revision_prefix "lr$lr-" \
         --output_dir "/data/checkpoint/$run_name"
 }
