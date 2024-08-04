48dd0efbf5aae30cb8cfee4511f87dc292a0e990
HeegyuKim <heekue83@gmail.com>
2024-08-04 05:06:00 +0000

    setup

--- tpu_flax_setup.sh
+++ tpu_flax_setup.sh
@@ -1,5 +1,6 @@
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install fjformer==0.0.69
 pip install -r requirements.txt
 pip install -U flax transformers gradio
